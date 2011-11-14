9e5176d5373b4cd1d88dbdbcc28525a650f0e0fb
Ookey Lai <ookey.lai@twgrid.org>
2011-11-14 13:42:40 +0000

    git-svn-id: svn+ssh://svn.cern.ch/reps/adcsw/adcmon/PandaBrokerageMonitor/trunk@996 7c6e8ea0-0a27-4360-9bb0-0d873ab685f0

--- charts_genV2.sh
+++ charts_genV2.sh
@@ -2,7 +2,4 @@
 source /home/ookey/.bash_profile
 
 cd /home/ookey/workspace/PandaBrokerageMonitor
-/usr/bin/python charts_weekV2.py > /dev/null 2>&1
-/usr/bin/python charts_monthV2.py > /dev/null 2>&1
-/usr/bin/python charts_seasonV2.py > /dev/null 2>&1
-/usr/bin/python charts_yearV2.py > /dev/null 2>&1
+/usr/bin/python charts_allV2.py > /dev/null 2>&1
