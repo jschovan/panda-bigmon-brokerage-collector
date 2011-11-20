24f3b89ba1f40ef81232d8932a5a881ea9b607fb
Jaroslava Schovancova <jschovan@gmail.com>
2011-11-20 19:50:10 +0000

    PandaBrokerageMonitor -- adding helpers/verify_values
    git-svn-id: svn+ssh://svn.cern.ch/reps/adcsw/adcmon/PandaBrokerageMonitor/trunk@1027 7c6e8ea0-0a27-4360-9bb0-0d873ab685f0

---
+++ helpers/verify_values/verifyDB.py
@@ -0,0 +1,96 @@
+import cx_Oracle, sys
+from datetime import datetime
+ 
+class dailyDB(object):
+    
+    _db = None
+    _connect = 'PandaBrokerageMonitor_ookey/PandaBrokerageMonitor2@devdb11'
+    
+    def __init__(self):       
+        # self._db = cx_Oracle.connect('PandaBrokerageMonitor_ookey/PandaBrokerageMonitor2@devdb11')
+        self._db = cx_Oracle.connect(self._connect)
+        
+    def print_tables(self):
+        # dailyLogV2
+        cursor = self._db.cursor()
+
+        """
+        #sql = "SELECT owner, table_name FROM all_tables"
+
+        sql = "SELECT table_name FROM user_tables"
+
+        #sql = "SELECT table_name FROM user_tables"
+        #sql = "SELECT * FROM LastUpdated"
+        #sql = "SELECT * FROM dailyLog LIMIT 10"
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        """
+
+        """
+        for tt in res[:1]:
+            t=tt[0]
+            print t
+            sql="DESCRIBE %s" % ( t )
+            #sql="DESCRIBE dailyLogV2"
+            print sql
+            cursor.execute(sql)
+            r=cursor.fetchall()
+            print t, r
+
+        """
+        """
+        sql="SELECT COUNT(*) FROM dailyLogV2"
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        print u'CMD:', sql, u'\nRESULT:', res
+        """
+        """	
+        sql="SELECT * FROM dailyLogV2 where ROWNUM<10"
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        print u'cursor.description', cursor.description
+        print u'CMD:', sql, u'\nRESULT:', res
+        """
+
+        sql = "SELECT table_name FROM user_tables"
+        cursor.execute(sql)
+        tables_tmp= cursor.fetchall()
+        tables=[x[0] for x in tables_tmp ]
+
+
+        for t in tables:
+            f='/tmp/jschovan/%s.py' % t
+            print u'dump', t, f; sys.stdout.flush()
+            fo=open(f,'w')
+            sql="SELECT * FROM %s -- where ROWNUM<10" % t
+            cursor.execute(sql)
+            tab_res = cursor.fetchall()
+            cur_desc=cursor.description
+            
+            fo.write("# CMD: \"%s\"\n#     RESULT LENGTH %d\n#     EXECUTED at %s\n" % (sql, len(tab_res), datetime.utcnow()))
+            fo.write("# COLUMNS: %s\n" % cur_desc)
+            fo.write("%s = %s\n" % (t, tab_res))
+            fo.close()
+
+        res=tab_res
+
+
+
+        cursor.close()
+        return res
+        
+    def query(self,sql):
+        cursor = self._db.cursor()
+        cursor.execute(sql)
+        rs = cursor.fetchall()
+        cursor.close()
+        return rs
+
+
+db=dailyDB()
+
+print u'### db.print_tables()'
+print db.print_tables()
+
+print u'EOF'
+
