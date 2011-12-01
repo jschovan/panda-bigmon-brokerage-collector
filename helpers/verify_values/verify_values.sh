913dbfbffd6fbb8c77ea00d73bf1c3ad984105fc
Jaroslava Schovancova <jschovan@gmail.com>
2011-12-01 13:39:43 +0000

    verify_values -- migrate to atlas-adcmon
    git-svn-id: svn+ssh://svn.cern.ch/reps/adcsw/adcmon/PandaBrokerageMonitor/trunk@1095 7c6e8ea0-0a27-4360-9bb0-0d873ab685f0

--- helpers/verify_values/verify_values.sh
+++ helpers/verify_values/verify_values.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # run on adcmon.cern.ch
 
-# adcmon
+MYPYTHON=/usr/bin/python2.5
+
+### adcmon
 WORKDIR=/data/jschovan/PandaBrokerageMon/verify_values
-PUBDIR=/var/www/html/PandaBrokerageMon/verify_values
-# lxplus
-WORKDIR=/afs/cern.ch/user/j/jschovan/scratch0/PandaBrokerageMon/verify_values
-PUBDIR=${WORKDIR}/pub
+#PUBDIR=/var/www/html/PandaBrokerageMon/verify_values
+PUBDIR=/data/adcmon-preproduction/PandaBrokerageMon/verify_values.test
+
+### lxplus
+#WORKDIR=/afs/cern.ch/user/j/jschovan/scratch0/PandaBrokerageMon/verify_values
+#PUBDIR=${WORKDIR}/pub
 
 LOGDIR=${WORKDIR}/logs
 DATESTRING=$(date +%F.%H%M%S)
@@ -19,18 +23,20 @@ cd ${WORKDIR}
 echo "Running at ${DATESTRING}."
 
 # get data from DB
-/usr/bin/time python verifyDB.py ${WORKDIR} 2>&1 | tee ${LOGDIR}/log.verifyDB.${DATESTRING}
+/usr/bin/time ${MYPYTHON} verifyDB.py ${WORKDIR} 2>&1 | tee ${LOGDIR}/log.verifyDB.${DATESTRING}
 
 # process data
-/usr/bin/time python verify_values.py ${WORKDIR} 2>&1 | tee ${LOGDIR}/log.verify_values.${DATESTRING}
+/usr/bin/time ${MYPYTHON} verify_values.py ${WORKDIR} 2>&1 | tee ${LOGDIR}/log.verify_values.${DATESTRING}
 
 # pack data
 cd ${WORKDIR}
+rm DAILYLOGV2.py.bz2 LASTUPDATEDV2.py.bz2
+
 bzip2 DAILYLOGV2.py
-bzip2 DAILYLOG.py
-bzip2 LASTUPDATED.py
+#bzip2 DAILYLOG.py
+#bzip2 LASTUPDATED.py
 bzip2 LASTUPDATEDV2.py
-bzip2 MONTHLYLOG.py
+#bzip2 MONTHLYLOG.py
 
 
 # publish data
