f11629ec3260af724910487d9af525633c70bc1e
Jaroslava Schovancova <jschovan@gmail.com>
2011-12-12 11:09:16 +0000

    PandaBrokerageMonitor -- tuning scripts for migration to atlas-adcmon
    git-svn-id: svn+ssh://svn.cern.ch/reps/adcsw/adcmon/PandaBrokerageMonitor/trunk@1125 7c6e8ea0-0a27-4360-9bb0-0d873ab685f0

--- dailyCollectV2.sh
+++ dailyCollectV2.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #source /home/ookey/workspace/PandaBrokerageMonitor/pbm_profile
-
 #cd /home/ookey/workspace/PandaBrokerageMonitor
+
+set -x
+
+PYTHON_EXECUTABLE="/usr/bin/python2.5"
 
 source /data/adcpbm1/PandaBrokerageMonitor/PandaBrokerageMonitor.conf
 source ${WORKDIR}/pbm_profile
 cd ${WORKDIR}
 
-
 mkdir -p ${TMP_DIR}
 
 date >> ${WORKDIR}/logs/PBMonV2.log
-/usr/bin/python ${WORKDIR}/parse_analy_brokerage_logV2.py >> ${WORKDIR}/logs/PBMonV2.log 2>&1
-/usr/bin/python ${WORKDIR}/my_logfileV2.py > /dev/null 2>&1
+${PYTHON_EXECUTABLE} ${WORKDIR}/parse_analy_brokerage_logV2.py >> ${WORKDIR}/logs/PBMonV2.log 2>&1
+${PYTHON_EXECUTABLE} ${WORKDIR}/my_logfileV2.py > /dev/null 2>&1
 
