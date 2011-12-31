a7f4742e678fcab2addd8ef2da747b172f320ea4
Jaroslava Schovancova <jschovan@gmail.com>
2011-12-31 19:44:11 +0000

    PandaBrokerageMonitor -- logging in dailyCollectV2.sh
    git-svn-id: svn+ssh://svn.cern.ch/reps/adcsw/adcmon/PandaBrokerageMonitor/trunk@1177 7c6e8ea0-0a27-4360-9bb0-0d873ab685f0

--- dailyCollectV2.sh
+++ dailyCollectV2.sh
@@ -11,7 +11,18 @@ cd ${WORKDIR}
 
 mkdir -p ${TMP_DIR}
 
+TIMESTAMP=$(date '+%F %T' -u)
+
+
 date >> ${WORKDIR}/logs/PBMonV2.log
 ${PYTHON_EXECUTABLE} ${WORKDIR}/parse_analy_brokerage_logV2.py >> ${WORKDIR}/logs/PBMonV2.log 2>&1
+
+### Deal with unresolved filesystems
+if [ -f "/tmp/adcpbm1/pbm_pidfile" ]; then 
+### mail to ADMIN
+    ADMIN="adcpbm1@cern.ch"
+    echo -e "$(basename $0) INFO [${TIMESTAMP} UTC] Please check logs and remove PIDfile (/tmp/adcpbm1/pbm_pidfile)" | mail -s "[PandaBrokerageMonitor] $(basename $0) - PID file stale lock" ${ADMIN} 
+fi
+
 ${PYTHON_EXECUTABLE} ${WORKDIR}/my_logfileV2.py > /dev/null 2>&1
 
