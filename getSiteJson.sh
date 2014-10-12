e69a1e094a53cec174f8ef10c32c025c8a8fcd7e
Jaroslava Schovancova <jschovan@gmail.com>
2014-10-12 23:52:12 +0000

    Updated getSiteJson URL to take json from AGIS.
    git-svn-id: svn+ssh://svn.cern.ch/reps/adcsw/adcmon/PandaBrokerageMonitor/trunk@2476 7c6e8ea0-0a27-4360-9bb0-0d873ab685f0

--- getSiteJson.sh
+++ getSiteJson.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "$(basename $0) $(date) start"
 source /data/adcpbm1/PandaBrokerageMonitor/PandaBrokerageMonitor.conf
-#cd /home/ookey/workspace/PandaBrokerageMonitor
 cd ${WORKDIR}
 /bin/rm -f ${WORKDIR}/panda_queues.json*
-/usr/bin/wget -q http://adc-ssb.cern.ch/SITE_EXCLUSION/panda_queues.json -O ${WORKDIR}/panda_queues.json
+# /usr/bin/wget -q http://adc-ssb.cern.ch/SITE_EXCLUSION/panda_queues.json -O ${WORKDIR}/panda_queues.json
+/usr/bin/wget -q "http://atlas-agis-api.cern.ch/request/pandaqueue/query/list/?json&preset=ssb" -O ${WORKDIR}/panda_queues.json
 echo "$(basename $0) $(date) finish"
 
