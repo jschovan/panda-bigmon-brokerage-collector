e4608d3171183fd127bf4c8384d6f37202ec1df0
Jaroslava Schovancova <jschovan@gmail.com>
2014-10-12 22:02:56 -0400

    Fixing paths.

--- pbm_collector/get_site/getSiteJson.sh
+++ pbm_collector/get_site/getSiteJson.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$(basename $0) $(date) start"
-source /data/adcpbm1/PandaBrokerageMonitor/PandaBrokerageMonitor.conf
+source /data/adcpbm1/lib/python2.6/site-packages/pbm_collector/PandaBrokerageMonitor.conf
 cd ${WORKDIR}
 /bin/rm -f ${WORKDIR}/panda_queues.json*
 # /usr/bin/wget -q http://adc-ssb.cern.ch/SITE_EXCLUSION/panda_queues.json -O ${WORKDIR}/panda_queues.json
