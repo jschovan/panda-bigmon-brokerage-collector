ed7cb9d41bfafcdb90b1b384bd267ae86b39e377
Jaroslava Schovancova <jschovan@gmail.com>
2014-10-12 23:16:23 -0400

    permissions

--- pbm_collector/get_site/getSiteJson.sh
+++ pbm_collector/get_site/getSiteJson.sh
@@ -3,7 +3,6 @@ echo "$(basename $0) $(date) start"
 source /data/adcpbm1/lib/python2.6/site-packages/pbm_collector/settings/PandaBrokerageMonitor.conf
 cd ${WORKDIR}
 /bin/rm -f ${WORKDIR}/settings/panda_queues.json*
-# /usr/bin/wget -q http://adc-ssb.cern.ch/SITE_EXCLUSION/panda_queues.json -O ${WORKDIR}/panda_queues.json
 /usr/bin/wget -q "http://atlas-agis-api.cern.ch/request/pandaqueue/query/list/?json&preset=ssb" -O ${WORKDIR}/settings/panda_queues.json
 echo "$(basename $0) $(date) finish"
 
